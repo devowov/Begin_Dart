1f89d2e20b99c681f9ec48a2c9c3401bbe0a6f13
EUNJU HWANG <dev.owov@gmail.com>
2023-05-10 15:56:48 +0900

    Cascade Notation

--- main.dart
+++ main.dart
@@ -1,32 +1,29 @@
 class Player {
   String name, team;
   int xp;
-  String job = 'dev';
 
   Player({required this.name, required this.xp, required this.team});
 
-  Player.createBluePlayer({required String name})
-      : this.xp = 0,
-        this.name = name,
-        this.team = 'blue';
-
-  Player.createBlackPlayer(String name)
-      : this.xp = 0,
-        this.name = name,
-        this.team = 'black';
-
   void sayHi() {
-    print("Hi! i'm ($team)${job}_${this.name} - lvl $xp");
+    print("Hi! i'm ($team)_${this.name} - lvl $xp");
   }
 }
 
 void main() {
-  var player = Player(name: 'ej', xp: 2000, team: 'red');
-  player.sayHi();
+  // var ej = Player(name: 'ej', xp: 1200000, team: 'wcc');
+  // ej.name = 'tester';
+  // ej.xp = 5000;
+  // ej.team = 'gst';
 
-  var bluePlayer = Player.createBluePlayer(name: 'tori');
-  bluePlayer.sayHi();
-
-  var blackPlayer = Player.createBlackPlayer('tomas');
-  blackPlayer.sayHi();
+  // cascade notation (.. = ej)
+  var ej = Player(name: 'ej', xp: 1200000, team: 'wcc');
+  // ..name = 'tester'
+  // ..xp = 5000
+  // ..team = 'gst';
+  // or
+  var potato = ej
+    ..name = 'tester'
+    ..xp = 5000
+    ..team = 'gst'
+    ..sayHi();
 }
