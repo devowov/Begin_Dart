a70a71ad6ef5175b7dea0f9b5a6466c6e94dd901
EUNJU HWANG <dev.owov@gmail.com>
2023-05-10 14:20:53 +0900

    Use Named Paramter

--- main.dart
+++ main.dart
@@ -1,15 +1,22 @@
-String sayHi() {
-  return 'hi there!';
+String sayHello(String name, int age, String country) {
+  return "Hello $name and $age, you 're $country isn't it?";
 }
 
-String sayHiToYou(String name) {
-  return 'hi $name!';
+String sayHelloNamed(
+    {String name = 'none', int age = 10, String country = 'kr'}) {
+  return "Hello $name and $age, you 're $country isn't it?";
 }
 
-String sayHiToYouDiff(String name) => 'hi $name!';
+String sayHelloNamed2(
+    {required String name, required int age, required String country}) {
+  return "Hello $name and $age, you 're $country isn't it?";
+}
 
-void main() {
-  print(sayHi());
-  print(sayHiToYou(':)'));
-  print(sayHiToYou('o0o'));
+// named
+void main(List args) {
+  print(sayHello("david", 19, "winter"));
+
+  print(sayHelloNamed(age: 19, name: "david"));
+
+  // print(sayHelloNamed2(age: 19, name: "david"));
 }
