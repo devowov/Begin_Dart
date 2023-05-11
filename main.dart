01d63ff14e3832d43402a78dfdb8f225a0ec6959
EUNJU HWANG <dev.owov@gmail.com>
2023-05-11 11:14:28 +0900

    use Abstract class

--- main.dart
+++ main.dart
@@ -1,6 +1,10 @@
+abstract class Human {
+  void walk();
+}
+
 enum Team { red, blue }
 
-class Player {
+class Player extends Human {
   String name;
   Team team;
   int xp;
@@ -10,9 +14,15 @@ class Player {
   void sayHi() {
     print("Hi! i'm ($team)_${this.name} - lvl $xp");
   }
+
+  @override
+  void walk() {
+    print("I'm walking 10 to 16");
+  }
 }
 
 void main() {
   var ej = Player(name: 'ej', xp: 1200000, team: Team.red);
   ej.sayHi();
+  ej.walk();
 }
