bb9dedcb36d04a1eea3d222034944b7cc68abb0c
EUNJU HWANG <dev.owov@gmail.com>
2023-05-11 11:26:56 +0900

    learn Inheritance of Dart

--- main.dart
+++ main.dart
@@ -1,28 +1,31 @@
-abstract class Human {
-  void walk();
+class Human {
+  final String name;
+  // Human(this.name);
+  Human({required this.name});
+  sayHi() {
+    print("Hi! my name is $name!");
+  }
 }
 
-enum Team { red, blue }
+enum Team { blue, red }
 
 class Player extends Human {
-  String name;
-  Team team;
-  int xp;
+  final Team team;
 
-  Player({required this.name, required this.xp, required this.team});
+  // super를 통한 (확장한) 부모 클래스와 상호 작용
+  // Player({required this.team, required String name}) : super(name);
+  Player({required this.team, required String name}) : super(name: name);
 
-  void sayHi() {
-    print("Hi! i'm ($team)_${this.name} - lvl $xp");
-  }
-
+  // if 상속 받은 클래스의 함수를 커스터마이징 하려면
   @override
-  void walk() {
-    print("I'm walking 10 to 16");
+  sayHi() {
+    // TODO: implement sayHi
+    super.sayHi();
+    print("and I paly for ${team}");
   }
 }
 
 void main() {
-  var ej = Player(name: 'ej', xp: 1200000, team: Team.red);
-  ej.sayHi();
-  ej.walk();
+  var player = Player(team: Team.red, name: 'ej');
+  player.sayHi();
 }
