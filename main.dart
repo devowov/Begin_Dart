02aae4f96a7ddda2fab475fe60a250fdae055485
EUNJU HWANG <dev.owov@gmail.com>
2023-05-11 15:04:34 +0900

    use Mixin

--- main.dart
+++ main.dart
@@ -1,31 +1,22 @@
-class Human {
-  final String name;
-  // Human(this.name);
-  Human({required this.name});
-  sayHi() {
-    print("Hi! my name is $name!");
+class Strong {
+  final double strengthLevel = 1500.99;
+}
+
+class QucikRunner {
+  void run() {
+    print('Hey..! runnnnnnnnnnnnnnnnn');
   }
 }
 
 enum Team { blue, red }
 
-class Player extends Human {
+class Player with Strong, QucikRunner {
   final Team team;
-
-  // super를 통한 (확장한) 부모 클래스와 상호 작용
-  // Player({required this.team, required String name}) : super(name);
-  Player({required this.team, required String name}) : super(name: name);
-
-  // if 상속 받은 클래스의 함수를 커스터마이징 하려면
-  @override
-  sayHi() {
-    // TODO: implement sayHi
-    super.sayHi();
-    print("and I paly for ${team}");
-  }
+  Player({required this.team, required String name});
 }
 
 void main() {
   var player = Player(team: Team.red, name: 'ej');
-  player.sayHi();
+  print(player.strengthLevel);
+  player.run();
 }
